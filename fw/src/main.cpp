bd6897973f2773229a630b0499c0089dcf7383be
gbox3d <gbox3d@gmail.com>
2025-05-17 21:37:05 +0900

    pulse actuator

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -120,13 +120,23 @@ int getTriggerCount() {
     return TriggerCounter::getTriggerCount();
 }
 
-// 탄약 수 감소 함수
+// 펄스 종료를 위한 태스크 선언
+Task task_EndPulse(TASK_IMMEDIATE, TASK_ONCE, []() {
+    digitalWrite(actionPin1, LOW);  // 펄스 종료 (액츄에이터 비활성화)
+}, &g_ts, false);  // 초기에는 비활성화 상태
+
+// 수정된 decreaseAmmoCount 함수
 void decreaseAmmoCount() {
     currentAmmoCount--;
     if (currentAmmoCount <= 0) {
         currentAmmoCount = 0;
         firingEnabled = false;
-        digitalWrite(actionPin1, HIGH);  // 액츄에이터 활성화하여 발사 중지
+        
+        // 펄스 시작
+        digitalWrite(actionPin1, HIGH);  // 액츄에이터 활성화
+        
+        // 1초 후에 펄스 종료 태스크 예약
+        task_EndPulse.restartDelayed(1000);
     }
 }
 
@@ -137,12 +147,12 @@ bool isMagazineInserted() {
 
 void resumeFiring() {
     firingEnabled = true;  // 발사 가능 상태로 설정
-    digitalWrite(actionPin1, LOW);  // 액츄에이터 비활성화
+    // digitalWrite(actionPin1, LOW);  // 액츄에이터 비활성화
 }
 
 void stopFiring() {
     firingEnabled = false;  // 발사 불가능 상태로 설정
-    digitalWrite(actionPin1, HIGH);  // 액츄에이터 활성화하여 발사 중지
+    // digitalWrite(actionPin1, HIGH);  // 액츄에이터 활성화하여 발사 중지
 }
 
 
@@ -201,7 +211,7 @@ Task task_NeoPixelBlink(500, TASK_FOREVER, []() {
     updateNeoPixelColor();
 }, &g_ts, true);
 
-Task taskNotify(20, TASK_FOREVER, []() {
+Task taskNotify(10, TASK_FOREVER, []() {
     static int oldValue = 0;
     static bool oldMagazineInserted = false;
     int _value = TriggerCounter::getTriggerCount();
